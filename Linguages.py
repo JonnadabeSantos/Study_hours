0c3b082d7a415be549758fa3321e9734807b83a2
JonnadabeSantos <129608615+JonnadabeSantos@users.noreply.github.com>
2023-10-05 10:56:30 -0300

    constrution_othe_linguages

--- Linguages.py
+++ Linguages.py
@@ -158,19 +158,15 @@ while True:
                        
                        
                         elif opc == 2:
-                           
-
-
-
+                            
                             while True:
                                 
                                 if EndProgram or returnMainMenu:
                                     break
 
                                 try:
-                                    linha()
-                                    cabeçalho(f'{keylist[ opc -1 ]} language selected !')
-                                    resp = input(f'\nWant to add hours {keylist[ opc -1 ]} language? [Y/N]?: ') [0]
+                                    print(cabeçalho(f'{keylist[ opc -1 ]} language selected !'))
+                                    resp = input(f'Want to add hours {keylist[ opc -1 ]} language? [Y/N]?: ') [0]
                                 except:
                                     print('\n\033[31m\nERROR ! Invalid value \033[m')
                                     sleep(1)
@@ -183,22 +179,36 @@ while True:
 
                                             print()                    
                                             print()                    
-                                            cabeçalho(f'Adding hours to {keylist[ opc -1 ]} studies')
+                                            print(cabeçalho(f'Adding hours to {keylist[ opc -1 ]} studies'))
                                             for key, value in registoGeral.items():
                                                 if key == keylist[ opc -1 ]:                                    
                                                     print(f'{key:<33}{value} Hours')
                                             try:
                                                 addhours = int(input('Enter the number of hours: '))
+                                                addminite = int(input('Enter the number of minutes: '))
+                                                addTime = studyTime( addhours, addminite, registoGeral[keylist[ opc - 1] ])
+
                                             except(ValueError):
-                                                print('\n\033[31m\nERROR ! Invalid value \033[m')
+                                                print('\n\033[31m\nERROR ! Invalid value*** \033[m')
                                                 sleep(1)
                                             else:                                                
-                                                registoGeral[ keylist[ opc -1 ] ] = ( int(registoGeral[ keylist[ opc -1 ] ]) + addhours )
-                                                # registoGeral[[keylist[ opc -1 ]] = int(registoGeral[[keylist[ opc -1 ]]) + int(input('Enter the number of hours: '))
-                                                updateDict( arq,registoGeral )
-                                                print(f'Successfully adding {addhours} hours to your {keylist[ opc - 1 ]} studies')
+                                                print( f'{register_hours( addhours, addminite, addTime )} {keylist[ opc - 1 ]} studies' )
+                                                reg_now = f'{register_hours( addhours, addminite, addTime, True )} - {keylist[ opc - 1 ]} studies'
 
+                                                registoGeral[ keylist[ opc -1 ] ] = addTime
+                                                updateDict( arq,registoGeral )
+
+                                                file_linguage = f'Fake_bank/Hours_linguages/{keylist[ opc - 1]}.txt'                                            
+                                                if not verificarArquivo(file_linguage) and ( addhours or addminite ) != 0:                                                 
+                                                    criarArquivo( file_linguage, cabeçalho( f"{keylist[ opc - 1]} language time recording", 114 ) )                                                                                                 
+                                                    updateHours( file_linguage, reg_now )
+                                               
+                                                else:
+                                                    if not 'Sorry' in reg_now:                                                 
+                                                        updateHours( file_linguage, reg_now )
+                                                        
                                                 
+
                                                 while True:                     
                                                     try:
                                                         returned = input('\nReturn to main menu [Y/N]: ') [0]
@@ -237,35 +247,196 @@ while True:
 
                                     else:
                                         print('\nIvalid Option ! Type YES or NO')
-                                        sleep(1)
+                                        sleep(1)                                
 
 
 
+                        elif opc == 3:
+                            
+                            while True:
+                                
+                                if EndProgram or returnMainMenu:
+                                    break
 
+                                try:
+                                    print(cabeçalho(f'{keylist[ opc -1 ]} language selected !'))
+                                    resp = input(f'Want to add hours {keylist[ opc -1 ]} language? [Y/N]?: ') [0]
+                                except:
+                                    print('\n\033[31m\nERROR ! Invalid value \033[m')
+                                    sleep(1)
+                                else:
+                                    if resp in 'Yy':
+                                        while True:
 
+                                            if EndProgram or returnMainMenu:
+                                                break
 
+                                            print()                    
+                                            print()                    
+                                            print(cabeçalho(f'Adding hours to {keylist[ opc -1 ]} studies'))
+                                            for key, value in registoGeral.items():
+                                                if key == keylist[ opc -1 ]:                                    
+                                                    print(f'{key:<33}{value} Hours')
+                                            try:
+                                                addhours = int(input('Enter the number of hours: '))
+                                                addminite = int(input('Enter the number of minutes: '))
+                                                addTime = studyTime( addhours, addminite, registoGeral[keylist[ opc - 1] ])
 
+                                            except(ValueError):
+                                                print('\n\033[31m\nERROR ! Invalid value*** \033[m')
+                                                sleep(1)
+                                            else:                                                
+                                                print( f'{register_hours( addhours, addminite, addTime )} {keylist[ opc - 1 ]} studies' )
+                                                reg_now = f'{register_hours( addhours, addminite, addTime, True )} - {keylist[ opc - 1 ]} studies'
 
+                                                registoGeral[ keylist[ opc -1 ] ] = addTime
+                                                updateDict( arq,registoGeral )
 
+                                                file_linguage = f'Fake_bank/Hours_linguages/{keylist[ opc - 1]}.txt'                                            
+                                                if not verificarArquivo(file_linguage) and ( addhours or addminite ) != 0:                                                 
+                                                    criarArquivo( file_linguage, cabeçalho( f"{keylist[ opc - 1]} language time recording", 108 ) )                                                                                                 
+                                                    updateHours( file_linguage, reg_now )
+                                               
+                                                else:
+                                                    if not 'Sorry' in reg_now:                                                 
+                                                        updateHours( file_linguage, reg_now )
+                                                        
+                                                
 
+                                                while True:                     
+                                                    try:
+                                                        returned = input('\nReturn to main menu [Y/N]: ') [0]
+                                                    except:
+                                                        print('\033[31m\nERROR ! Invalid value \033[m')
+                                                    else:
+                                                        if returned in 'Yy':
+                                                            returnMainMenu = True
+                                                            break
+                                                        elif returned in 'Nn':
 
+                                                            EndProgram = True # used to end all whiles
+                                                            break
+                                                        else:
+                                                            print('\nIvalid Option ! Type YES or NO')
+                                                            sleep(1)                                              
+                                                                                               
 
+                                    elif resp in 'Nn':
+                                        while True:                     
+                                            try:
+                                                returned = input('Return to main menu [Y/N]: ') [0]
+                                            except:
+                                                print('\033[31m\nERROR ! Invalid value \033[m')
+                                            else:
+                                                if returned in 'Yy':
+                                                    returnMainMenu = True
+                                                    break
+                                                elif returned in 'Nn':
 
-                        elif opc == len( linguages ) + 1:
-                            break
+                                                    EndProgram = True # used to end all whiles
+                                                    break
+                                                else:
+                                                    print('\nIvalid Option ! Type YES or NO')
+                                                    sleep(1)                                                   
 
-                        elif opc == len( linguages ) + 2:
-                            EndProgram = True                            
-                            break
-                     
-
-                    
-                    else:
-                        print(f'\n\033[33mInvalid Number !! a number between 1 and {len(mainMenu) + 1}\n\033[m')
-                        sleep(2)
+                                    else:
+                                        print('\nIvalid Option ! Type YES or NO')
+                                        sleep(1)                                                           
                 
                 
-        
+                        
+                        
+                        elif opc == 4:
+                            
+                            while True:
+                                
+                                if EndProgram or returnMainMenu:
+                                    break
+
+                                try:
+                                    print(cabeçalho(f'{keylist[ opc -1 ]} language selected !'))
+                                    resp = input(f'Want to add hours {keylist[ opc -1 ]} language? [Y/N]?: ') [0]
+                                except:
+                                    print('\n\033[31m\nERROR ! Invalid value \033[m')
+                                    sleep(1)
+                                else:
+                                    if resp in 'Yy':
+                                        while True:
+
+                                            if EndProgram or returnMainMenu:
+                                                break
+
+                                            print()                    
+                                            print()                    
+                                            print(cabeçalho(f'Adding hours to {keylist[ opc -1 ]} studies'))
+                                            for key, value in registoGeral.items():
+                                                if key == keylist[ opc -1 ]:                                    
+                                                    print(f'{key:<33}{value} Hours')
+                                            try:
+                                                addhours = int(input('Enter the number of hours: '))
+                                                addminite = int(input('Enter the number of minutes: '))
+                                                addTime = studyTime( addhours, addminite, registoGeral[keylist[ opc - 1] ])
+
+                                            except(ValueError):
+                                                print('\n\033[31m\nERROR ! Invalid value*** \033[m')
+                                                sleep(1)
+                                            else:                                                
+                                                print( f'{register_hours( addhours, addminite, addTime )} {keylist[ opc - 1 ]} studies' )
+                                                reg_now = f'{register_hours( addhours, addminite, addTime, True )} - {keylist[ opc - 1 ]} studies'
+
+                                                registoGeral[ keylist[ opc -1 ] ] = addTime
+                                                updateDict( arq,registoGeral )
+
+                                                file_linguage = f'Fake_bank/Hours_linguages/{keylist[ opc - 1]}.txt'                                            
+                                                if not verificarArquivo(file_linguage) and ( addhours or addminite ) != 0:                                                 
+                                                    criarArquivo( file_linguage, cabeçalho( f"{keylist[ opc - 1]} language time recording", 106 ) )                                                                                                 
+                                                    updateHours( file_linguage, reg_now )
+                                               
+                                                else:
+                                                    if not 'Sorry' in reg_now:                                                 
+                                                        updateHours( file_linguage, reg_now )
+                                                        
+                                                
+
+                                                while True:                     
+                                                    try:
+                                                        returned = input('\nReturn to main menu [Y/N]: ') [0]
+                                                    except:
+                                                        print('\033[31m\nERROR ! Invalid value \033[m')
+                                                    else:
+                                                        if returned in 'Yy':
+                                                            returnMainMenu = True
+                                                            break
+                                                        elif returned in 'Nn':
+
+                                                            EndProgram = True # used to end all whiles
+                                                            break
+                                                        else:
+                                                            print('\nIvalid Option ! Type YES or NO')
+                                                            sleep(1)                                              
+                                                                                               
+
+                                    elif resp in 'Nn':
+                                        while True:                     
+                                            try:
+                                                returned = input('Return to main menu [Y/N]: ') [0]
+                                            except:
+                                                print('\033[31m\nERROR ! Invalid value \033[m')
+                                            else:
+                                                if returned in 'Yy':
+                                                    returnMainMenu = True
+                                                    break
+                                                elif returned in 'Nn':
+
+                                                    EndProgram = True # used to end all whiles
+                                                    break
+                                                else:
+                                                    print('\nIvalid Option ! Type YES or NO')
+                                                    sleep(1)                                                   
+
+                                    else:
+                                        print('\nIvalid Option ! Type YES or NO')
+                                        sleep(1)                              
         
         elif menuSelect == 2:
             print('\nStatistic')
